44a6385c33a2821e5276bd14f1f2a2672c1b4047
agalotaibi <hopes.gmo@gmail.com>
2026-02-15 18:06:02 +0000

    solve task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
+"""
+    Module with typing Python
+"""
+
+
 def add(a: float, b: float) -> float:
     """
-    thie model return the sum
-    of a and ba as float
+        Args:
+            a: The first parameter
+            b: The second parameter
+
+        Return:
+            Sum of two floats
+
     """
     return a + b
