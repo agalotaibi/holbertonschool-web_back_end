df0c1865f4236a2d4bced34d1215e80f5547b733
agalotaibi <hopes.gmo@gmail.com>
2026-02-17 18:06:12 +0000

    solve task 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,13 +5,14 @@ to return concatenated string
 """
 
 
-def concat(str1: str, str2: str)-> str:
+def concat (str1: str, str2: str) -> str:
     """
     Arges:
     str1: string pass to the function
-    str2: string pass to the function  
+    str2: string pass to the function
 
     Return:
     concatenated string
     """
+
     return str1 + str2
