4f7ad2529c394791ae3ca7f2b1772f7db15fc72b
agalotaibi <hopes.gmo@gmail.com>
2026-02-17 18:01:10 +0000

    solve task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Model about using type-annotated 
+to return concatenated string
+"""
+
+def concat(str1: String, str2: String)-> String:
+    """
+    Arges:
+    str1: string pass to the function
+    str2: string pass to the function  
+
+    Return:
+    concatenated string
+    """
+    return str1 + str2
