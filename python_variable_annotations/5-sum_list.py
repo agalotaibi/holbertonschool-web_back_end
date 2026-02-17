b1479e598bf19e77085e92fb29ee88fa84b4119f
agalotaibi <hopes.gmo@gmail.com>
2026-02-17 18:23:54 +0000

    solve task 3

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+cx#!/usr/bin/env python3
+"""
+Model return list sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Args:
+    input_list: passing arrgument
+    return:
+    sum of lisr as float
+    """
+
+    return sum(input_list)
