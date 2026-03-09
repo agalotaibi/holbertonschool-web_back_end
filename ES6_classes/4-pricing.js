37c735460993be719de4a825333f823b6f2c8a33
agalotaibi <hopes.gmo@gmail.com>
2026-03-09 18:03:28 +0000

    solve task 4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -22,11 +22,11 @@ class Pricing {
   }
 
   set currency(value) {
-    // Use 'instanceof' to check if 'value' is an instance of the 'Currency' class
+
     if (!(value instanceof Currency)) {
       throw new TypeError('currency must be a Currency');
     }
-    this._currency = value; // Assign 'value', not the argument 'currency'
+    this._currency = value;
   }
 
   displayFullPrice() {
