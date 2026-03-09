a366499b088dd13eb9721c4be4cf87b71f3a3021
agalotaibi <hopes.gmo@gmail.com>
2026-03-09 18:41:39 +0000

    solve task 10

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,43 +1,41 @@
 class Car {
-    constructor(brand, motor, color) {
-	this.brand = brand;
-	this.motor = motor;
-	this.color = color;
-    }
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
 
-    get brand() { return this._brand; }
-    set brand(value) {
-	if (typeof value !== 'string') throw new TypeError('brand must be a string');
-	this._brand = value;
-    }
+  get brand() { return this._brand; }
+  set brand(value) {
+    if (typeof value !== 'string') throw new TypeError('brand must be a string');
+    this._brand = value;
+  }
 
-    get motor() { return this._motor; }
-    set motor(value) {
-	if (typeof value !== 'string') throw new TypeError('motor must be a string');
-	this._motor = value;
-    }
+  get motor() { return this._motor; }
+  set motor(value) {
+    if (typeof value !== 'string') throw new TypeError('motor must be a string');
+    this._motor = value;
+  }
 
-    get color() { return this._color; }
-    set color(value) {
-	if (typeof value !== 'string') throw new TypeError('color must be a string');
-	this._color = value;
-    }
+  get color() { return this._color; }
+  set color(value) {
+    if (typeof value !== 'string') throw new TypeError('color must be a string');
+    this._color = value;
+  }
 
-    static get [Symbol.species]() {
-	return this;
-    }
+  static get [Symbol.species]() {
+    return this;
+  }
 
-    cloneCar() {
-	const clone = Object.create(Object.getPrototypeOf(this));
-
-	const props = Object.getOwnPropertyNames(this);
-
-	props.forEach((key) => {
-	    clone[key] = this[key];
-	});
-
-	return clone;
-    }
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    
+    const clone = Object.create(Species.prototype);
+    
+    Object.assign(clone, this);
+    
+    return clone;
+  }
 }
 
 export default Car;
