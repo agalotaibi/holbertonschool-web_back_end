a16361f1de62633ba0de03998df7d75d21094a00
agalotaibi <hopes.gmo@gmail.com>
2026-03-09 18:37:40 +0000

    solve task 10

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -5,42 +5,38 @@ class Car {
     this.color = color;
   }
 
-  get brand() {
-    return this._brand;
-  }
+  get brand() { 
+    return this._brand; 
+}
+
   set brand(value) {
     if (typeof value !== 'string') throw new TypeError('brand must be a string');
     this._brand = value;
   }
 
-  get motor() {
-    return this._motor;
-  }
+  get motor() { 
+    return this._motor; 
+}
   set motor(value) {
     if (typeof value !== 'string') throw new TypeError('motor must be a string');
     this._motor = value;
   }
 
-  get color() {
-    return this._color;
-  }
+  get color() { 
+    return this._color; 
+}
   set color(value) {
     if (typeof value !== 'string') throw new TypeError('color must be a string');
     this._color = value;
   }
 
-    cloneCar() {
-    const clone = Object.create(Object.getPrototypeOf(this));
+  static get [Symbol.species]() {
+    return this;
+  }
 
-    const props = Object.getOwnPropertyNames(this);
-    const symbols = Object.getOwnPropertySymbols(this);
-    const allKeys = [...props, ...symbols];
-
-    allKeys.forEach((key) => {
-        clone[key] = this[key];
-    });
-
-    return clone;
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species(this.brand, this.motor, this.color);
   }
 }
 
