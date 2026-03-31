31b4a419933d0a1270db8a3dfe716d41fb355149
agalotaibi <hopes.gmo@gmail.com>
2026-03-31 07:24:21 +0000

    solve task 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const grocMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ]);
+  return grocMap;
+}
