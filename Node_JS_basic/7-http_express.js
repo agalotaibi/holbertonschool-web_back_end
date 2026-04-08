ac78038f90cc520a15582f6d592121064b3cefed
agalotaibi <hopes.gmo@gmail.com>
2026-04-08 15:39:32 +0000

    solve task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,25 +2,21 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const PORT = 1245;
-const DATABASE = process.argv[2];
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-
-app.get('/students', async (req, res) => {
-  res.write('This is the list of our students\n');
-  
-  try {
-    const data = await countStudents(DATABASE);
-    res.end(data);
-  } catch (error) {
-    res.end(error.message);
-  }
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((message) => {
+      res.send(`This is the list of our students\n${message}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
 });
 
-app.listen(PORT);
+app.listen(1245);
 
 module.exports = app;
