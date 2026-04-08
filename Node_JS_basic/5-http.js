40cce513381e0a8037faa087858c9a88df1e4631
agalotaibi <hopes.gmo@gmail.com>
2026-04-08 14:56:50 +0000

    solve task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,33 +1,26 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = process.argv[2];
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-const port = 1245;
-
-const app = http.createServer(async (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-
-    if (req.url === '/') {
-	res.statusCode = 200;
-	res.end('Hello Holberton School!');
-    }
-    else if (req.url === '/students') {
-	res.statusCode = 200;
-	res.write('This is the list of our students\n');
-	try {
-	    const studentsData = await countStudents(DATABASE);
-	    res.end(studentsData);
-	} catch (error) {
-	    res.end(error.message);
-	}
-    }
-    else {
-	res.statusCode = 404;
-	res.end('Not Found');
-    }
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    
+    // Use the argument passed in terminal
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(data);
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
+  }
 });
 
-app.listen(port);
+app.listen(1245);
 
 module.exports = app;
